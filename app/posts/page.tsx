71a9477404cb9c2d2611c7355cb1acee9a7d54be
Ngochanako <147359823+Ngochanako@users.noreply.github.com>
2024-09-04 11:32:34 +0700

    l

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -5,7 +5,6 @@ import React, { Suspense, useEffect, useState } from 'react'
 export default function page() {
     const [search,setSearch]=useState<string>('');
     const [valueSearch,setValueSearch]=useState<string>('');
-    const searchParam=useSearchParams();
     const router=useRouter();
     const handleChange=(e:React.ChangeEvent<HTMLInputElement>)=>{
         setSearch(e.target.value);
@@ -13,16 +12,27 @@ export default function page() {
     const handleClick=()=>{
         router.push(`/posts?search=${search}`);
     }
-    useEffect(()=>{
-        let search=searchParam.get('search');
-        if(search)setValueSearch(search);
-    },[searchParam])
+    const SearchInput = ()=>{
+      const searchParam=useSearchParams();
+      useEffect(()=>{
+          let search=searchParam.get('search');
+          if(search)setValueSearch(search);
+      },[searchParam])
+      return(
+        <>
+        <input onChange={handleChange} value={search} className='border-solid border-2  border-gray-400 p-[10px]' type="text"  />
+         <button onClick={handleClick} className='bg-black text-white rounded-[5px] p-[5px]'>Tìm kiếm</button>
+        </>
+      )
+    }
   return (
-    <Suspense fallback={<div>Loading ...</div>}>
+    
     <div>
-      <input onChange={handleChange} value={search} className='border-solid border-2  border-gray-400 p-[10px]' type="text"  /> <button onClick={handleClick} className='bg-black text-white rounded-[5px] p-[5px]'>Tìm kiếm</button>
+      <Suspense fallback={<div>Loading ...</div>}>
+      <SearchInput/>
+      </Suspense>
       <p>tìm kiếm:{valueSearch}</p>
     </div>
-    </Suspense>
+    
   )
 }
